03ff65966c0f43d8dee9aa65460f48e5e861129c
vs222qj <vs222qj@student.lnu.se>
2022-08-14 19:08:52 +0200

    added cors for the express app

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -16,6 +16,10 @@ import http from 'http'
 
 dotenv.config()
 const app = express()
+
+app.use(cors({
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3000']
+}))
 const httpServer = http.createServer(app)
 
 /**
