1b0b1d53ba551e3e84fd9897f271bbde3ddb1db0
vs222qj <vs222qj@student.lnu.se>
2022-08-14 19:12:39 +0200

    added cors requirement

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -16,10 +16,6 @@ import http from 'http'
 
 dotenv.config()
 const app = express()
-
-app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000']
-}))
 const httpServer = http.createServer(app)
 
 /**
@@ -30,26 +26,13 @@ const main = async () => {
   const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
   const connection = await mysql.createConnection(process.env.DATABASE_URL!)
 
-  let socketPath
   if (process.env.NODE_ENV === 'production') {
     app.set('trust proxy', 1)
-    //socketPath = '/cloudsql/fluted-curve-350720:europe-west9:space-shooter'
     app.use(cors({
       origin: [FRONTEND_URL],
       credentials: true,
     }))
   }
-
-  /*
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'space_shooter',
-    socketPath
-  })
-  connection.connect()
-  */
 
   app.use(
     session({
@@ -86,8 +69,9 @@ const main = async () => {
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
   })
 
-  const corsOptions = {
-    origin: [FRONTEND_URL]
+  const corsOptions: cors.CorsOptions = {
+    origin: [FRONTEND_URL],
+    credentials: true
   }
 
   await server.start()
