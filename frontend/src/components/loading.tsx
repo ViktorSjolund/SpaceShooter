e1f9794738fe0145fad5a1427b6de67d0985d4f1
vs222qj <vs222qj@student.lnu.se>
2022-08-18 23:51:52 +0200

    change loading screen

--- frontend/src/components/loading.tsx
+++ frontend/src/components/loading.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react'
+
 export const Loading = () => {
+  let [dots, setDots] = useState('')
+
+  const handleDots = () => {
+    const dot = '.'
+    const maxDots = 3
+    let i = 1
+
+    setInterval(() => {
+      if (i === maxDots + 1) {
+        i = 1
+      }
+      setDots(dot.repeat(i))
+      i++
+    }, 500)
+  }
+
+  useEffect(() => {
+    handleDots()
+  }, [])
+
   return (
-    <div className='loading'>
-      <h1> Loading... </h1>
-      <img src='logo192.png' alt=''></img>
+    <div className='wrapper'>
+      <div className='loading'>
+        <h1>Loading{dots}</h1>
+        <img src='logo192.png' alt=''></img>
+      </div>
     </div>
   )
 }
