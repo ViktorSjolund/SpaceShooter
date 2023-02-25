5653d84e1872e2b36f90091a19f4d5b4a7aa57e6
vs222qj <vs222qj@student.lnu.se>
2023-02-25 01:32:30 +0100

    changed how loading screen looks

--- frontend/src/components/loading.tsx
+++ frontend/src/components/loading.tsx
@@ -1,42 +1,11 @@
-import { useEffect, useState } from 'react'
-
 export const Loading = () => {
-  let [dots, setDots] = useState('.')
-  let [dotInterval, setDotInterval] = useState<NodeJS.Timer | null>(null)
-
-  const handleDots = () => {
-    const dot = '.'
-    const maxDots = 3
-    let i = 1
-
-    setDotInterval(
-      setInterval(() => {
-        if (i > maxDots) {
-          i = 1
-        }
-        setDots(dot.repeat(i))
-        i++
-      }, 500)
-    )
-  }
-
-  useEffect(() => {
-    handleDots()
-
-    return () => {
-      if (dotInterval) {
-        clearInterval(dotInterval)
-      }
-    }
-  }, [dotInterval])
-
   return (
     <div className='wrapper'>
       <div className='loading'>
-        <h1>Loading{dots}</h1>
         <img
           src='logo192.png'
-          alt=''></img>
+          alt=''
+        ></img>
       </div>
     </div>
   )
